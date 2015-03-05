569844d70e58fff8ac44b03281e93f2a3d309670
LIL717 <lhill7@me.com>
2015-03-05 11:07:31 -0800

    remove extra 40 pt margin for iPad (which fixes back scrolling issue on iPhone) add background image to Root View Controller add blur effect with tint to background image

--- Ceaseless/RootViewController.h
+++ Ceaseless/RootViewController.h
@@ -7,9 +7,15 @@
 //
 
 #import <UIKit/UIKit.h>
+	//RGB color macro with alpha
+#define UIColorFromRGBWithAlpha(rgbValue,a) [UIColor \
+colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 \
+green:((float)((rgbValue & 0xFF00) >> 8))/255.0 \
+blue:((float)(rgbValue & 0xFF))/255.0 alpha:a]
 
 @interface RootViewController : UIViewController <UIPageViewControllerDelegate>
 
+@property (weak, nonatomic) IBOutlet UIImageView *backgroundImageView;
 @property (strong, nonatomic) UIPageViewController *pageViewController;
 
 @end
