299f4ed5512e9180c967a77e1546c35b94639d6b
LIL717 <lhill7@me.com>
2015-03-10 15:22:18 -0700

    added Core Data code to appDelegate

--- Ceaseless/AppDelegate.h
+++ Ceaseless/AppDelegate.h
@@ -7,9 +7,17 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
 #import "Scripture.h"
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
 
 @property (strong, nonatomic) UIWindow *window;
 @property (nonatomic, strong) NSArray *peopleArray;
