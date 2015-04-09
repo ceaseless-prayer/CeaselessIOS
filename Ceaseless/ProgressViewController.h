08423b7449af684572186a56b0c65c8be8225fcf
Chris <kirisu@gmail.com>
2015-04-08 18:53:48 -0700

    adding an optional and rudimentary ProgressView as the last card for a user to see how far they have gone through their Ceaseless contacts

---
+++ Ceaseless/ProgressViewController.h
@@ -0,0 +1,14 @@
+//
+//  ProgressViewController.h
+//  Ceaseless
+//
+//  Created by Christopher Lim on 4/8/15.
+//  Copyright (c) 2015 Christopher Lim. All rights reserved.
+//
+
+#import "DataViewController.h"
+#import "ProgressView.h"
+
+@interface ProgressViewController : DataViewController
+    @property (strong, nonatomic) IBOutlet ProgressView *progressView;
+@end
---
+++ Ceaseless/ProgressViewController.m
@@ -0,0 +1,40 @@
+//
+//  ProgressViewController.m
+//  Ceaseless
+//
+//  Created by Christopher Lim on 4/8/15.
+//  Copyright (c) 2015 Christopher Lim. All rights reserved.
+//
+
+#import "ProgressViewController.h"
+#import "AppUtils.h"
+
+@interface ProgressViewController ()
+
+@end
+
+@implementation ProgressViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.progressView.backgroundImageView.image = [AppUtils getDynamicBackgroundImage];
+    [self formatCardView: self.progressView.cardView withShadowView:self.progressView.shadowView];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
